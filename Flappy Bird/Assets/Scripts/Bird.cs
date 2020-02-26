7dd6503f283c6d8d6d7c793ae1c39508db0adaca
Thomas Hughes <thomas1hughes@aol.com>
2020-02-25 23:14:53 -0500

    Post Git Upload
    Completed Project

---
+++ Flappy Bird/Assets/Scripts/Bird.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+
+    public float upForce = 200f;
+    private bool isDead = false;
+    private Animator anim;        //Reference to the Animator component.
+    private Rigidbody2D rb2d;     //Holds a reference to the Rigidbody2D component of the bird.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get reference to the Animator component attached to this GameObject.
+        anim = GetComponent<Animator> ();
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDead)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D (){ //Unity is always looking for this function on a collision
+        rb2d.velocity = Vector2.zero;
+        isDead = true;
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
+    }
+}
---
+++ Flappy Bird/Assets/Scripts/GameControl.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+
+    public static GameControl instance; //A reference to our game control script so we can access it statically.
+    public Text scoreText;
+    public GameObject gameOverText; //A reference to the object that displays the text which appears when the player dies.
+
+    private int score = 0;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if(instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy (gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameOver && Input.GetMouseButtonDown(0)){
+            //...reload the current scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
+        }
+    }
+
+    public void BirdScored(){
+        if(gameOver) return;
+        score++;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+}
